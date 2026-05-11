f80ee91c134ba700e29ed369542a955ebbd0e757
Ambrosio Cadena <acadena@magnumtires.com.br>
2026-05-11 11:59:33 -0300

    Adiciona login com Supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://cvgvrijlsjyqqzrztsqv.supabase.co";
-const supabaseAnonKey = "sb_publishable_LMSx2y13bWR5SsQiwLQL6g_OyQCGHx2";
+const supabaseAnonKey = "SUA_CHAVE_PUBLICAVEL_COMPLETA";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
